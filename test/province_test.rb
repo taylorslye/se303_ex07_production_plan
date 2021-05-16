60ad053360bf46e455e58a09504a0e77d42c2e7e
Taylor Slye <slyet@oregonstate.edu>
2021-05-16 16:05:57 -0700

    wrapped tests in describe province

--- test/province_test.rb
+++ test/province_test.rb
@@ -6,21 +6,24 @@ require_relative '../lib/data'
 
 class ProvinceTest < Minitest::Test
 
-  def setup
-    @asia = Province.new(sample_province_data)
+  describe 'province' do
+    def setup
+      @asia = Province.new(sample_province_data)
+    end
+
+    def test_province_shortfall
+      assert_equal(5, @asia.shortfall)
+    end
+
+    def test_province_proft
+      assert_equal(230, @asia.profit)
+    end
+
+    def test_province_change_production
+      @asia.producers[0].production = 20
+      assert_equal(-6, @asia.shortfall)
+      assert_equal(292, @asia.profit)
+    end
   end
 
-  def test_province_shortfall
-    assert_equal(5, @asia.shortfall)
-  end
-
-  def test_province_proft
-    assert_equal(230, @asia.profit)
-  end
-
-  def test_province_change_production
-    @asia.producers[0].production = 20
-    assert_equal(-6, @asia.shortfall)
-    assert_equal(292, @asia.profit)
-  end
 end
