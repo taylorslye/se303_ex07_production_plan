90bfb21b4045bac6bd2534483edb21e45f44922d
Taylor Slye <slyet@oregonstate.edu>
2021-05-16 15:48:23 -0700

    add province profit test

--- test/province_test.rb
+++ test/province_test.rb
@@ -10,4 +10,9 @@ class ProvinceTest < Minitest::Test
     asia = Province.new(sample_province_data)
     assert_equal(5, asia.shortfall)
   end
+
+  def test_province_proft
+    asia = Province.new(sample_province_data)
+    assert_equal(230, asia.profit)
+  end
 end
